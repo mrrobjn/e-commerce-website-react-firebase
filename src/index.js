c4d5a268e0ad63fc63f1c6ffab998eca766d79dc
Võ Trường Dũng <87188466+mrrobjn@users.noreply.github.com>
2023-04-04 18:45:02 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ProductProvider } from './context/ProductContext';
